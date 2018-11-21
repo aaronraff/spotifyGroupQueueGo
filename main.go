29f460c1d5834adb614310921e32ead5f65a568e
Aaron Raff <aaronraffdev@gmail.com>
2018-11-21 16:53:05 -0500

    Add Procfile for Heroku

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
+	// For Heroku
+	port := os.Getenv("PORT")
+	if port == nil {
+		port = 8080
+	}
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":" + port, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
