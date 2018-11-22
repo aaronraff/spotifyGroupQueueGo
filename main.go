da2ec10b67c7d3988db179ac1bf1d66eaa214847
Aaron Raff <aaronraffdev@gmail.com>
2018-11-22 13:13:12 -0500

    Add Spotify login redirect

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -12,8 +13,19 @@ func main() {
 		port = "8080"
 	}
 
+	http.HandleFunc("/login", loginHandler);
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":" + port, nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	clientId := os.Getenv("CLIENTID")
+	redirectUri:= url.QueryEscape("http://localhost:8080/")
+	scopes := url.QueryEscape("user-modify-playback-state user-read-playback-state")
+	url := "https://accounts.spotify.com/authorize"	+
+		   "?response_type=code" + "&client_id=" + clientId +
+		   "&scope=" + scopes + "&redirect_uri=" + redirectUri
+	http.Redirect(w, r, url, http.StatusSeeOther)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
