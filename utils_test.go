cdfdbfb1cd7eee1d8c30b8431010fc4e43791b9d
Aaron Raff <aaronraffdev@gmail.com>
2019-01-28 22:16:26 -0500

    Remove obsolete test

--- utils_test.go
+++ utils_test.go
@@ -4,33 +4,7 @@ import (
 	"fmt"
 	"testing"
 	"github.com/zmb3/spotify"
-	"golang.org/x/oauth2"
 )
-
-func TestGetTokenFromCode(t *testing.T) {
-	Rooms["user1"] = RoomInfo { code: "abc", tok: &oauth2.Token { AccessToken: "token1" }}
-	Rooms["user2"] = RoomInfo { code: "bcd", tok: &oauth2.Token { AccessToken: "token2" }}
-	Rooms["user3"] = RoomInfo { code: "cde", tok: &oauth2.Token { AccessToken: "token3" }}
-
-	testCases := []struct {
-					code string
-					expected *oauth2.Token
-				}{
-					{	"bcd", Rooms["user2"].tok },
-					{	"zfg", nil },
-				}
-
-	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("code: %s, expected: %v", tc.code, tc.expected),
-			func(t *testing.T) {	
-				actual := GetTokenFromCode(tc.code)
-
-				if actual != tc.expected {
-					t.Errorf("got %v, want %v", actual, tc.expected)
-				}
-		})
-	}
-}
 
 func TestIsSongPresent(t *testing.T) {
 	tracks := []spotify.PlaylistTrack {
