fbb993de86a594aacdd512bc83a8eba47f55733b
Aaron Raff <aaronraffdev@gmail.com>
2019-04-08 14:01:33 -0400

    Add documentation to /sessionStore/store.go

--- sessionStore/store.go
+++ sessionStore/store.go
@@ -10,14 +10,19 @@ func init() {
 	store = make(map[string]*oauth2.Token)
 }
 
+// RegisterUser adds a uuid along with that user's Spotify token to the session
+// store.
 func RegisterUser(uuid string, tok *oauth2.Token) {
 	store[uuid] = tok
 }
 
+// DestroyUser removes the entry with the given uuid from the session store.
 func DestroyUser(uuid string) {
 	delete(store, uuid)
 }
 
+// GetToken gets the Spotify token associated with the given uuid from the
+// session store.
 func GetToken(uuid string) *oauth2.Token {
 	return store[uuid]
 }
