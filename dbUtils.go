bbfdd237a76dd8a5b1ac69e958ee865be88a872a
Aaron Raff <aaronraffdev@gmail.com>
2019-04-08 13:58:12 -0400

    Add documentation to dbUtils.go

--- dbUtils.go
+++ dbUtils.go
@@ -23,6 +23,8 @@ func init() {
 	encryptionKey = key
 }
 
+// GetAllRoomCodes looks retrieves all of the roomCodes from the database
+// which is effectively all of the open rooms.
 func GetAllRoomCodes(db *sql.DB) []string {
 	rows, err := db.Query("SELECT room_code FROM rooms")
 	defer rows.Close()
@@ -50,6 +52,8 @@ func GetAllRoomCodes(db *sql.DB) []string {
 	return res
 }
 
+// GetTokenFromCode retrieves the Spotify API token for the specified roomCode
+// from the database and decrypts it.
 func GetTokenFromCode(db *sql.DB, roomCode string) *oauth2.Token {	
 	row := db.QueryRow("SELECT oauth_token FROM rooms WHERE room_code = $1", roomCode)
 
@@ -77,6 +81,8 @@ func GetTokenFromCode(db *sql.DB, roomCode string) *oauth2.Token {
 	return tok
 }
 
+// DoesRoomExist checks whether or not a specified roomCode exists in the
+// database (which indicates it is an open room).
 func DoesRoomExist(db *sql.DB, roomCode string) bool {	
 	row := db.QueryRow("SELECT user_id from rooms WHERE room_code = $1", roomCode)
 
@@ -88,6 +94,7 @@ func DoesRoomExist(db *sql.DB, roomCode string) bool {
 	return true
 }
 
+// GetRoomCode gets the roomCode from the database for the specified userID.
 func GetRoomCode(db *sql.DB, userID string) string {
 	row := db.QueryRow("SELECT room_code from rooms WHERE user_id = $1", userID)
 
@@ -103,6 +110,8 @@ func GetRoomCode(db *sql.DB, userID string) string {
 	return *roomCode
 }
 
+// DeleteRoom deletes the user's (specified by their userID) room from the
+// database.
 func DeleteRoom(db *sql.DB, userID string) {
 	_, err := db.Exec("DELETE FROM rooms WHERE user_id = $1", userID)
 
@@ -111,6 +120,9 @@ func DeleteRoom(db *sql.DB, userID string) {
 	}
 }
 
+// InsertRoom inserts a row into the rooms table in the database. It inserts
+// a roomCode, userID (room host), and encrypts the given oauth token before
+// storing it.
 func InsertRoom(db *sql.DB, roomCode string, userID string, tok *oauth2.Token) {
 	tokJ, err := json.Marshal(tok)	
 	tokS := string(tokJ)
